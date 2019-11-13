1480b31a36eeef4728d6a292f9361c968b7dcd7e
Dustin Cummings <dustincummings15@gmail.com>
2019-11-13 09:28:37 -0500

    Added pagination, filtering, and sorting

--- DatingApp-SPA/src/app/members/members-list/members-list.component.ts
+++ DatingApp-SPA/src/app/members/members-list/members-list.component.ts
@@ -29,6 +29,7 @@ export class MembersListComponent implements OnInit {
   this.userParams.gender = this.user.gender ==='female'?'male' : 'female';
   this.userParams.minAge = 18;
   this.userParams.maxAge = 99;
+  this.userParams.orderBy = 'lastActive';
   }
   pageChanged(event: any) : void {
     this.pagination.currentPage = event.page;
