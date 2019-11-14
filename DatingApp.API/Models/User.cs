190c0093865a063acbf041e3987ffbd6c7bc2636
Dustin Cummings <dustincummings15@gmail.com>
2019-11-14 09:45:37 -0500

    Added the Messages Entity to the Database

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -22,5 +22,7 @@ namespace DatingApp.API.Models
         public ICollection<Photo> Photos { get; set; }
         public ICollection<Like> Likers { get; set; }
         public ICollection<Like> Likees { get; set; }
+        public ICollection<Messages> MessagesSent { get; set; }
+        public ICollection<Messages> MessagesReceived { get; set; }
     }
 }
