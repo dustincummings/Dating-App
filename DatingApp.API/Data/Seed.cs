2aac7d8e4a292338f8ad07d678986fce69901389
Dustin Cummings <dustincummings15@gmail.com>
2019-12-02 11:23:53 -0500

    Added roles to the Seed user method

--- DatingApp.API/Data/Seed.cs
+++ DatingApp.API/Data/Seed.cs
@@ -10,16 +10,39 @@ namespace DatingApp.API.Data
     {
         
 
-        public static void SeedUsers(UserManager<User> userManager)
+        public static void SeedUsers(UserManager<User> userManager, RoleManager<Role> roleManager)
         {
             if(!userManager.Users.Any())
             {
                 var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
                 var users = JsonConvert.DeserializeObject<List<User>>(userData);
+
+                var roles = new List<Role>
+                {
+                    new Role{Name = "Member"},
+                    new Role{Name = "Admin"},
+                    new Role{Name = "Moderator"},
+                    new Role{Name = "VIP"},
+                };
+                foreach(var role in roles)
+                {
+                    roleManager.CreateAsync(role).Wait();
+                }
                 foreach (var user in users)
                 {
                     userManager.CreateAsync(user,"password").Wait();
-                }                
+                    userManager.AddToRoleAsync(user, "Member");
+                }   
+
+                var adminUser = new User{UserName= "Admin"};
+                var result = userManager.CreateAsync(adminUser, "password").Result;
+
+                if(result.Succeeded)
+                {
+                    var admin = userManager.FindByNameAsync("Admin").Result;
+                    userManager.AddToRolesAsync(adminUser, new []{"Admin", "Moderator"});
+                }
+                              
             }    
         }
           private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
